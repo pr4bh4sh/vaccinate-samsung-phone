740ca2e71fba5e1efdbe499e08c31ae00ef6e7d2
Prabhash <pr4bh4sh@users.noreply.github.com>
2019-10-11 14:14:23 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+adb -d shell pm uninstall -k --user 0 com.samsung.android.oneconnect
+adb -d shell pm uninstall -k --user 0 com.sec.android.app.sbrowser
+adb -d shell pm uninstall -k --user 0 com.sec.android.app.shealth
+adb -d shell pm uninstall -k --user 0 com.samsung.android.app.watchmanager
+adb -d shell pm uninstall -k --user 0 com.facebook.services
+adb -d shell pm uninstall -k --user 0 com.samsung.android.voc
+adb -d shell pm uninstall -k --user 0 com.facebook.appmanager
+adb -d shell pm uninstall -k --user 0 com.sec.android.app.samsungapps
+adb -d shell pm uninstall -k --user 0 com.sec.android.easyMover
