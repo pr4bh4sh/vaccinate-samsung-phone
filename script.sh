051d6613f36f222604373767b28d5da941504c35
Prabhash <pr4bh4sh@users.noreply.github.com>
2020-01-03 12:19:37 +0000

    adding more trash to clean

--- script.sh
+++ script.sh
@@ -1,11 +1,22 @@
+echo 'cleaning samsung\'s shit pile'
 adb -d shell pm uninstall -k --user 0 com.samsung.android.oneconnect
 adb -d shell pm uninstall -k --user 0 com.sec.android.app.sbrowser
 adb -d shell pm uninstall -k --user 0 com.sec.android.app.shealth
 adb -d shell pm uninstall -k --user 0 com.samsung.android.app.watchmanager
 adb -d shell pm uninstall -k --user 0 com.facebook.services
 adb -d shell pm uninstall -k --user 0 com.samsung.android.voc
+adb -d shell pm uninstall -k --user 0 com.sec.android.app.samsungapps
+adb -d shell pm uninstall -k --user 0 com.sec.android.easyMover
+
+echo 'cleaning bloatwares'
+adb -d shell pm uninstall -k --user 0 com.spotify.music
 adb -d shell pm uninstall -k --user 0 com.facebook.appmanager
 adb -d shell pm uninstall -k --user 0 com.facebook.katana
-adb -d shell pm uninstall -k --user 0 com.sec.android.app.samsungapps
-adb -d shell pm uninstall -k --user 0 com.spotify.music
-adb -d shell pm uninstall -k --user 0 com.sec.android.easyMover
+
+echo 'cleaning microsoft product'
+adb -d shell pm uninstall -k --user 0 com.microsoft.office.excel
+adb -d shell pm uninstall -k --user 0 com.microsoft.skydrive
+adb -d shell pm uninstall -k --user 0 com.microsoft.office.word
+adb -d shell pm uninstall -k --user 0 com.microsoft.office.powerpoint
+adb -d shell pm uninstall -k --user 0 com.skype.raider
+echo 'all done'
